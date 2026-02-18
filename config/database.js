93e3778b0bdbbfcf063cbef68e01504b7a66bcf7
Nick Scholz <nickscholz1003@gmail.com>
2026-02-17 22:32:36 -0500

    remove connect db log

--- config/database.js
+++ config/database.js
@@ -7,11 +7,6 @@ const pool = new Pool({
     }
 });
 
-// Test connection
-pool.on('connect', () => {
-  console.log('Connected to PostgreSQL database');
-});
-
 pool.on('error', (err) => {
   console.error('Unexpected error on idle client', err);
   process.exit(-1);
